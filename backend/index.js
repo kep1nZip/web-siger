694a4303d1465f929fbde15f731e63b1294349b8
kep1nZip <johaneskevina@gmail.com>
2026-02-23 20:28:35 +0700

    test cors lagi

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,16 @@ const PORT = process.env.PORT || 8080;
 // ✅ CORS CONFIG (PRODUCTION READY)
 // ===============================
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://web-sigers.vercel.app",
+      "https://web-sigers-wine.vercel.app" // GANTI kalau domain beda
+    ],
+    methods: ["GET", "POST", "DELETE"],
+  })
+);
 
 app.use(express.json());
 
