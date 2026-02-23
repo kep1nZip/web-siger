eee6155f793fe6244f95a089de8a1efa3713a25b
kep1nZip <johaneskevina@gmail.com>
2026-02-23 20:41:54 +0700

    revisi lagiiii

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const API_URL =
-  import.meta.env.VITE_API_URL ||"https://web-siger-production.up.railway.app" || "http://localhost:8080";
+  "https://web-siger-production.up.railway.app" || import.meta.env.VITE_API_URL || "https://web-siger-production.up.railway.app" || "http://localhost:8080";
